8d33e883f198f169a86a3d6c9fd21d9f96d1d127
jsichon <jsichon@gmail.com>
2016-04-14 18:59:44 -0400

    First commit

--- PhotoApp/CameraViewController.swift
+++ PhotoApp/CameraViewController.swift
@@ -1,35 +1,97 @@
-//
-//  CameraViewController.swift
-//  PhotoApp
-//
-//  Created by Sichon, Juan on 3/8/16.
-//  Copyright © 2016 Sichon, Juan. All rights reserved.
-//
-
 import UIKit
+import MobileCoreServices
 
-class CameraViewController: UIViewController {
+// Enums
+enum CameraMode {
+    case Rear
+    case Front
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+class CameraViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var rearImageView: UIImageView!
+    @IBOutlet weak var frontImageView: UIImageView!
+    
+    var newMedia: Bool?
+    
+    // Enum Parameters
+    func cameraPhoto(cameraMode : CameraMode) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+            //Enum Parameters to Pass
+            imagePicker.cameraDevice = cameraMode == CameraMode.Front ? .Front : .Rear
+            imagePicker.mediaTypes = [kUTTypeImage as NSString as String]
+            imagePicker.allowsEditing = false
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+            newMedia = true
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func rearCameraPhoto(sender: UIButton) {cameraPhoto(.Rear)}
+    
+    @IBAction func frontCameraPhoto(sender: UIButton) {
+        cameraPhoto(.Front)
     }
-    */
+    @IBAction func frontImage(sender: UITapGestureRecognizer) {
+        cameraPhoto(.Front)
+    }
 
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if picker.cameraDevice == .Front {
+            picker.dismissViewControllerAnimated(true, completion: nil)
+            frontImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+            frontImageView.clipsToBounds = true
+            frontImageView.contentMode = UIViewContentMode.ScaleAspectFill
+            frontImageView.transform = CGAffineTransformMakeScale(-1, 1)
+        } else {
+            picker.dismissViewControllerAnimated(true, completion: nil)
+            rearImageView.transform = CGAffineTransformIdentity            
+            rearImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+            rearImageView.clipsToBounds = true
+            rearImageView.contentMode = UIViewContentMode.ScaleAspectFill
+        }
+    }
+    
+    //Need to figure out how to ScaleAspectFill and reverse Front Camera Photo
+    func getVerticalImg() -> UIImage {
+        let photoSize = CGSizeMake(rearImageView.image!.size.width, rearImageView.image!.size.width * 2)
+        //let photoSize = CGSizeMake(width: CGFloat, height: CGFloat)
+        
+        UIGraphicsBeginImageContext(photoSize)
+        
+        rearImageView.image!.drawInRect(CGRectMake(0, 0, photoSize.width, photoSize.width))
+        frontImageView.image!.drawInRect(CGRectMake(0, photoSize.width, photoSize.width, photoSize.width))
+        //rearImageView.image!.drawinRect(CGRectMake(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat)
+        
+        let finalImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return finalImage
+    }
+    
+    @IBAction func savePhoto(sender: UIButton) {
+        let finalMixedImage = getVerticalImg()
+        UIImageWriteToSavedPhotosAlbum(finalMixedImage, nil, nil, nil);
+        self.savedImageAlert()
+    }
+    
+    func savedImageAlert() {
+        let alertController = UIAlertController(title: "It worked!", message: "Your picture was saved to Camera Roll.", preferredStyle: .Alert)
+        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in}
+        alertController.addAction(OKAction)
+        self.presentViewController(alertController, animated: true) {}
+    }
+    
+    @IBAction func sharePhoto(sender: AnyObject) {
+        let finalMixedImage = getVerticalImg()
+        self.displayShareSheet(finalMixedImage)
+    }
+    
+    func displayShareSheet(image:UIImage) {
+        let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        presentViewController(activityViewController, animated: true, completion: {})
+    }
+    
 }
--- PhotoApp/onBoardingController.swift
+++ PhotoApp/onBoardingController.swift
@@ -1,25 +1,33 @@
-//
-//  ViewController.swift
-//  PhotoApp
-//
-//  Created by Sichon, Juan on 3/8/16.
-//  Copyright © 2016 Sichon, Juan. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class onBoardingController: UIViewController {
 
+    @IBOutlet weak var btnOnBoarding: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //btnOnBoarding Button Styles
+        btnOnBoarding.layer.borderColor = UIColor.blueColor().CGColor // Set border color
+        btnOnBoarding.layer.borderWidth = 1 // Set border width
+        btnOnBoarding.layer.borderColor = UIColor(red: 74/255, green: 144/255, blue: 226/255, alpha: 1).CGColor
+        btnOnBoarding.layer.cornerRadius = 10
+        btnOnBoarding.layer.backgroundColor = UIColor(red: 74/255, green: 144/255, blue: 226/255, alpha: 1).CGColor
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func goCamera(sender: AnyObject) {
+        
+    }
+
+
+    
 
 
 }
-
